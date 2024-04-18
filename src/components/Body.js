f96856a1fd33dacbdd9c86c17d2bc3790547fc0e
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-04-18 22:05:20 +0530

    added components

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainContainer from "./MainContainer";
+
+const Body = () => {
+  return (
+    <div>
+      <Sidebar />
+      <MainContainer />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Head.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <div>
+        <img
+          alt="menu"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+        />
+
+        <img
+          alt="youtube-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+        />
+      </div>
+      <div>
+        <input
+          className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Head;
